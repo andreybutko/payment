82a7a53492d71aaa42cca13ec3bb2ed0e21ba154
Andrii Butko <andrii.butko@eleks.com>
2020-11-29 12:00:52 +0200

    Set host name header to common middleware X-Server-Name contains host name in response headers

--- api/middleware/common.go
+++ api/middleware/common.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"os"
 	"github.com/codegangsta/negroni"
 	"net/http"
 )
@@ -8,6 +9,8 @@ import (
 // Common is base common middleware
 func Common() negroni.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request,  next http.HandlerFunc) {
+		name, _ := os.Hostname()
+		w.Header().Add("X-Server-Name", name)
 		w.Header().Add("Content-Type", "application/json")
 		next(w, r)
 	}
